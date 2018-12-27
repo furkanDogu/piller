b292d439b82d5f99f7ec4c4e7a2bd4c7b2a5b64e
Furkan Doğu <37083315+furkanDogu@users.noreply.github.com>
2018-12-28 00:53:33 +0300

    a

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,12 @@
 let mysql = require('mysql');
 let config = require('./db_config');
 
-let pool  = mysql.createPool(config);
+let pool  = mysql.createPool({
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    user: "b9ff67ddb3f48f",
+    password: "31551bae",
+    database: "heroku_2ed98b7fdb9a055"
+});
 
 var getConnection = (callback) => {
     pool.getConnection((err, connection) => {
