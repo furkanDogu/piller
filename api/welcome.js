29afa3e38924ba296204e189fd4a9a7239cf3e30
Furkan Doğu <37083315+furkanDogu@users.noreply.github.com>
2018-12-28 01:26:25 +0300

    a

--- api/welcome.js
+++ api/welcome.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const getConnection = require('../db/pool');
 
 router.get('/', (req, res) => {
     res.status(200).send('Hello fucking world!');
 });
 
+router.get('/emre', (req, res) => {
+    getConnection((connError, conn) => {
+        conn.query('SELECT * FROM firsttable', (err, result) => {
+            res.status(200).json(result);
+            conn.release();
+        });
+    });
+});
 module.exports = router;
