1f0a168cfbea97ad8b489bf3642abe265020b1c1
Furkan Doğu <37083315+furkanDogu@users.noreply.github.com>
2019-05-03 20:33:34 +0300

    Update db_config.js

--- db/db_config.js
+++ db/db_config.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: "us-cdbr-iron-east-01.cleardb.net",
     user: "b9ff67ddb3f48f",
-    password: "31551baes",
+    password: "31551bae",
     database: "heroku_2ed98b7fdb9a055"
 };
